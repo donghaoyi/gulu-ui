6c3a3c295fc2adbd37eb319e368b5653af4b5e06
donghaoyi <1203350867@qq.com>
2020-08-19 14:59:17 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      导航栏 | 
+      <router-link to="/">Hao</router-link> |
+      <router-link to="xxx">xxx</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+};
+</script>
