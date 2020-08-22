f102fcf2746418646130cf5b848474abd00c80fc
donghaoyi <1203350867@qq.com>
2020-08-22 16:49:55 +0800

    dom 结构

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+            <h1>轱辘UI</h1>
+            <h3>一个厉害的、UI框架</h3>
+            <p class="actions">
+                <a href="">GitHub</a>
+                <a href="">开始</a>
+            </p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'Home'
+}
+</script>
+<style lang="scss" scoped>
+    .topnav{
+        border: 1px solid red;
+    }
+</style>
