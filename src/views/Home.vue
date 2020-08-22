62d5d53b8e7edbee57e973e5ca06129375c2c5ab
donghaoyi <1203350867@qq.com>
2020-08-22 17:14:37 +0800

    topnav 样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
         <div class="topnav">
-            <div class="logo"></div>
-            <div class="menu"></div>
+            <div class="logo">LOGO</div>
+            <div class="menu">
+                <ul>
+                    <li>菜单 1</li>
+                    <li>菜单 2</li>
+                </ul>
+            </div>
         </div>
         <div class="banner">
             <h1>轱辘UI</h1>
@@ -21,6 +26,20 @@ export default {
 </script>
 <style lang="scss" scoped>
     .topnav{
-        border: 1px solid red;
+        display: flex;
+        padding: 16px;
+        background-color: pink;
+        > .logo{
+            max-width: 6em;
+            margin-right: auto;
+        }
+        > .menu{
+            display: flex;
+            flex-wrap: nowrap;
+            white-space: nowrap;
+            > li{
+                margin: 0 1em;
+            }
+        }
     }
 </style>
