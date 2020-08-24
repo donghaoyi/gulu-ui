b8b6aca97e183a7b046380742c860cc911a08ca0
donghaoyi <1203350867@qq.com>
2020-08-24 16:13:46 +0800

    添加 toggleAside

--- src/components/topnav.vue
+++ src/components/topnav.vue
@@ -1,10 +1,11 @@
 <template>
 <div class="topnav">
-    <div class="logo" @click="toggleMeun">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
         <li>菜单1</li>
         <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleMeun"></span>
 </div>
 </template>
 
@@ -12,25 +13,27 @@
 import {
     inject,
     Ref
-} from 'vue';
+} from "vue";
 export default {
     setup() {
-        const menuVisible = inject < Ref < boolean >> ('menuVisible')
-        console.log('topnav 获取的menuVisible的值是：', menuVisible);
+        const menuVisible = inject < Ref < boolean >> ("menuVisible");
+        console.log("topnav 获取的menuVisible的值是：", menuVisible);
         const toggleMeun = () => {
             menuVisible.value = !menuVisible.value;
-        }
-        return {
-            toggleMeun
         };
-    }
+        return {
+            toggleMeun,
+        };
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .topnav {
-    background: pink;
     display: flex;
+    justify-content: center;
+    align-items: center;
+    background: pink;
     padding: 16px;
     position: relative;
     z-index: 10;
@@ -47,6 +50,31 @@ export default {
 
         >li {
             margin: 0 1em;
+        }
+    }
+
+    >.toggleAside {
+        display: none;
+        width: 24px;
+        height: 24px;
+        background: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+    @media (max-width: 500px) {
+        >.menu {
+            display: none;
+        }
+
+        >.logo {
+            margin: 0 auto;
+        }
+
+        >.toggleAside {
+            display: inline-block;
         }
     }
 }
